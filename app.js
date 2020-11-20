6a2a8b787bf6d35b127091e22b1c817a951bd293
flsks <59883326+flsks@users.noreply.github.com>
2020-11-20 14:16:38 +0100

    little changes

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+/* app.use(express.static(path.join(__dirname, 'public')));
+ */
 
-
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/restaurants', restaurantsRouter);
 app.use('/cities', citiesRouter);
 app.use('/tags', tagsRouter);
