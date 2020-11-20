fbc2638f62fb3eda0d7784dfeb009d0cbbc112b7
flsks <59883326+flsks@users.noreply.github.com>
2020-11-20 15:22:45 +0100

    add some api routes

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+/* const path = require('path'); */
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const indexRouter = require('./routes/index');
-const restaurantsRouter = require('./routes/restaurants');
-const citiesRouter = require('./routes/cities');
-const tagsRouter = require('./routes/tags');
-const commentsRouter = require('./routes/comments');
+const indexRouter = require("./routes/index");
+const restaurantsRouter = require("./routes/restaurants");
+const citiesRouter = require("./routes/cities");
+const tagsRouter = require("./routes/tags");
+const commentsRouter = require("./routes/comments");
 
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 /* app.use(express.static(path.join(__dirname, 'public')));
  */
 
-app.use('/', indexRouter);
-app.use('/restaurants', restaurantsRouter);
-app.use('/cities', citiesRouter);
-app.use('/tags', tagsRouter);
-app.use('/comments', commentsRouter);
-
+app.use("/", indexRouter);
+app.use("/restaurants", restaurantsRouter);
+app.use("/cities", citiesRouter);
+app.use("/tags", tagsRouter);
+app.use("/comments", commentsRouter);
 
 module.exports = app;
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,32 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const client = require('../client/client')
+const client = require("../client/client");
 
 /* GET all restaurants page. */
 router.get("/", (req, res) => {
-  
-  client
-      .query("SELECT * FROM restaurant")
-      .then((data) => res.json(data.rows))
-      .catch((err) => res.sendStatus(500));
+	client
+		.query("SELECT * FROM restaurant")
+		.then((data) => res.json(data.rows))
+		.catch((err) => res.sendStatus(500));
 });
 
 /* GET all restaurants page. */
 router.get("/:id", (req, res) => {
-  const { id } = req.params; 
-  client
-      .query("SELECT * FROM restaurant WHERE id=$1", [id])
-      .then((data) => res.json(data.rows))
-      .catch((err) => res.sendStatus(500));
+	const { id } = req.params;
+	client
+		.query("SELECT restaurant.id restaurant_id, restaurant.name restaurant_name, city.name city_name, comment.id comment_id, comment.text comment_text, comment.date comment_date FROM restaurant LEFT JOIN comment ON restaurant.id = comment.restaurant_id RIGHT JOIN city ON restaurant.city_id = city.id WHERE restaurant_id=$1;", [id])
+		.then((data) => res.json(data.rows))
+		.catch((err) => res.sendStatus(500));
 });
 
 /* GET all restaurants page. */
 router.post("/", (req, res) => {
-  const { name, city_id } = req.body;
-  client
-      .query("INSERT INTO restaurant(name, city_id) VALUES($1, $2) RETURNING *", [name, city_id])
-      .then((data) => res.json(data.rows))
-      .catch((err) => res.sendStatus(500));
+	const { name, city_id } = req.body;
+	client
+		.query(
+			"INSERT INTO restaurant(name, city_id) VALUES($1, $2) RETURNING *",
+			[name, city_id]
+		)
+		.then((data) => res.json(data.rows))
+		.catch((err) => res.sendStatus(500));
 });
 
 module.exports = router;
